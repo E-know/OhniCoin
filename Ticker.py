6b8a7ee7d377125471f625e2fb3f3d4441f607c6
Inho Choi <eknow25@gmail.com>
2021-09-11 11:43:37 +0900

    KRW Ticker만 조회로 변경

--- Ticker.py
+++ Ticker.py
@@ -16,19 +16,22 @@ def get_ticker(count=None):
 	headers = {"Accept": "application/json"}
 	
 	response = requests.request("GET", url, headers=headers, params=querystring)
-	
+	'''
+	https://docs.upbit.com/reference#%EC%9D%BCday-%EC%BA%94%EB%93%A4-1
+	'''
 	ticker = []
 	print('Get Ticker from UPbit', end='')
 	for i, json_data in enumerate(response.json()):
-		candle = get_market_candle(json_data['market'])
-		node = dict(candle[0])
-		ticker.append(node)
+		if json_data['market'][:3] == 'KRW':
+			candle = get_market_candle(json_data['market'])
+			node = dict(candle[0])
+			ticker.append(node)
 		if i % 10 == 0:
 			time.sleep(1.1)
 			print('.', end='')
 	
 	print('DONE!')
-	ticker.sort(key=lambda it: -it['candle_acc_trade_price'])
+	ticker.sort(key=lambda it: -it['change_rate'])
 	
 	return ticker if count is None else ticker[:count]
 
